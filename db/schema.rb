a887680b6af67c3c8bb5b9a1d60528db08e2fdef
“hitoshi.nishimura” <nishimura198929@gmail.com>
2019-02-04 13:35:06 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20190128134614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
-    t.string   "title"
   end
 
 end
